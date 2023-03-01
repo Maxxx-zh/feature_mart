3cbe30c2d566d98e538c681c600670af22dc08be
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-03-01 19:46:57 +0200

    Update pipeline_public_weather_fg_actions.py

--- pipeline_public_weather_fg_actions.py
+++ pipeline_public_weather_fg_actions.py
@@ -110,6 +110,7 @@ def get_weather_data(city_name: str,
 def main():
     # Connect to Hopsworks FS
     print('Retrieving project...')
+    print(API_KEY)
     project = hopsworks.login(project='weather', api_key_value=API_KEY)
     print('Project retrieved')
     fs = project.get_feature_store() 
